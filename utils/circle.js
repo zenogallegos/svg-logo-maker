a856ea61cf1f6d866c7c3e9aa75b76ea383b28c5
Zeno Gallegos <zenogallegos@Zeno-MacBook-Pro.local>
2024-03-15 18:12:43 -0600

    created base files to begin creating the js files for the other shapes as well as for inquirer

---
+++ utils/circle.js
@@ -0,0 +1,16 @@
+const Shape = require("./shapeParent");
+
+class Circle extends Shape {
+  constructor(textInput, textColor, shapeColor) {
+    super(textInput, textColor, shapeColor);
+  }
+  render() {
+    return;
+    `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="150" cy="100" r="80" fill="green" />
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+        </svg>`;
+  };
+}
+
+module.exports = Circle;
---
+++ utils/shapeParent.js
@@ -0,0 +1,9 @@
+class ShapeParent {
+    constructor(textInput, textColor, shapeColor){
+        this.text = textInput,
+        this.textColor = textColor,
+        this.shapeColor = shapeColor
+    }
+}
+
+module.exports = ShapeParent;
